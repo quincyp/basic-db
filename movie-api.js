689c9d5b628f455edd6859eeccf6dd755f4715ce
quincyp <quincytpham@gmail.com>
2023-07-31 15:56:23 -0700

    Implemented an insert/update route, dot env still pending, uri removed for now

--- movie-api.js
+++ movie-api.js
@@ -61,19 +61,128 @@ app.route('/all')
     }
   })
   .delete(async (req, res) => {
-    movieList = [];
-    res.sendStatus(200)
+    let result = {};
+    let error = null;
+    try {
+      await client.connect();
+      const collection = client.db("basicdb").collection("inventory");
+      // TODO: UNCOMMENT WHEN READY TO IMPLEMENT DELETE ROUTE
+      result = await collection.deleteMany({});
+      console.log(result);
+    }
+    catch(e) {
+      console.dir(e)
+      error = e;
+    }
+    finally {
+      await client.close();
+    }
+    if(error === null) {
+      res.sendStatus(200);
+    } else {
+      res.status(500).send("Failure");
+    }
+
   })
 
-app.get('/find', (req, res) => {
-  console.log(req.query)
+app.get('/find', async (req, res) => {
+  let result = {};
+  let error = null;
   if (req.query.hasOwnProperty('contains')) {
-    res.json(movieList.filter((title) => title.toLowerCase().indexOf(req.query.contains.toLowerCase()) > -1))
-  }
-  else if (req.query.hasOwnProperty('startsWith')) {
-    res.json(movieList.filter((title) => title.toLowerCase().indexOf(req.query.startsWith.toLowerCase()) === 0))
+    try {
+      await client.connect();
+      const collection = client.db("basicdb").collection("inventory");
+      // Alternative regex
+      // result = await collection.find({
+      //   title: {
+      //     $regex: req.query.contains,
+      //     $options: 'i' // regex option i = ignore casing
+      //   }
+      // }).toArray();
+      result = await collection.find({
+          title: {
+              $regex: new RegExp(req.query.contains, 'i')
+            }
+          }).toArray();
+      console.log(result);
+
+      if(error === null) {
+        res.json(result.map((value) => {
+          return value.title;
+        }))
+      } else {
+        res.status(500).send("Failure");
+      }
+    }
+    catch (e) {
+      console.dir(e);
+      error = e;
+    }
+    finally {
+      await client.close();
+    }
+  } else if(req.query.hasOwnProperty('startsWith')) {
+    try {
+      await client.connect();
+      const collection = client.db("basicdb").collection("inventory");
+      result = await collection.find({
+        title: {
+          $regex: new RegExp(req.query.startsWith, 'i')
+        }
+      }).toArray();
+      console.log(result);
+
+      if(error === null) {
+        res.json(result.map((value) => {
+          return value.title;
+        }))
+      } else {
+        res.status(500).send("Failure");
+      }
+    }
+    catch (e) {
+      console.dir(e);
+      error = e;
+    }
+    finally {
+      await client.close();
+    }
   }
 })
+
+app.route('/insert')
+  .post(async (req, res) => {
+    let error = null;
+    let result = {};
+    try {
+      await client.connect();
+      const collection = client.db("basicdb").collection("inventory");
+      insertList = [
+        { "title": "The Avengers 2" },
+        { "title": "All Dogs Go To Heaven 2" },
+        { "title": "The Aristocats 2" },
+        { "title": "The Brave Little Toaster 2" },
+        { "title": "The Lord of the Rings 2" },
+        { "title": "The Revenant 2" },
+        { "title": "Cats & Dogs 2" }
+      ]
+      result = await collection.insertMany(insertList);
+      console.log(result);
+
+      if(error === null)  {
+        res.sendStatus(200);
+      } else {
+        res.status(500).send("Failure");
+      }
+    }
+    catch(e) {
+      console.dir(e);
+      error = e;
+    }
+    finally {
+      await client.close();
+    }
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
